7659ac0cb8a7429653a631104e96c48a99aac839
Rodrigo Salazar <rodrigosalad@gmail.com>
2023-05-12 13:27:58 -0500

    final prediction

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd 
 import time
+import random
 from config import *
 from testing import test_all, test_logistic, test_svm, compare_models
 from load import load_dataset, load_iris
@@ -15,6 +16,10 @@ x_train, x_test, y_train = load_dataset(N=20)
 # TESTING
 test_all(x_train, y_train, "DB", seed=42)
 compare_models(x_train, y_train, "DB", seed=42)
+
+# SET SEED
+random.seed(42)
+np.random.seed(42) 
 
 # BEST
 dt = DecisionTree(max_depth=None)
