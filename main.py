4efc3e2e65a45c2e5b0b8ca2fdf9b66fdd0c5543
Rodrigo Salazar <rodrigosalad@gmail.com>
2023-05-12 13:26:07 -0500

    final prediction

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd 
 import time
 from config import *
 from testing import test_all, test_logistic, test_svm, compare_models
@@ -11,10 +12,17 @@ np.set_printoptions(precision = 5, floatmode="fixed", suppress = True)
 # LOAD DATASET
 x_train, x_test, y_train = load_dataset(N=20)
 
-
-# TRAIN & TEST
+# TESTING
 test_all(x_train, y_train, "DB", seed=42)
 compare_models(x_train, y_train, "DB", seed=42)
+
+# BEST
+dt = DecisionTree(max_depth=None)
+dt.fit(x_train, y_train)
+prediction  = dt.predict(x_test).astype(int)
+linecounter = np.arange(len(prediction))
+res = np.array([linecounter, prediction.reshape(-1)]).T
+np.savetxt("output.csv",res,delimiter=" ",fmt='%d')
 
 #"""
 #Debug with iris
